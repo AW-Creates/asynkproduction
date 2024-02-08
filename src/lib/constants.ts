de87628e06f03c5be920f313849b19b2b2d7ccea
SncrlyYourz <55216710+SncrlyYourz@users.noreply.github.com>
2024-02-08 16:10:25 -0500

    Initial commit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -58,7 +58,7 @@ export const pricingCards = [
 ]
 
 export const addOnProducts = [
-  { title: 'Priority Support', id: 'prod_PNjJAE2EpP16pn' },
+  { title: 'Priority Support', id: 'prod_PWiATyMXu0smge' },
 ]
 
 export const icons = [
